c2ec984f48ddb1a47a7a9be7cdaa3e822537fdc5
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 12:44:31 -0300

    created JPA repository and database seeding

---
+++ src/main/java/com/industriaspleneo/course/repositories/IUserRepository.java
@@ -0,0 +1,8 @@
+package com.industriaspleneo.course.repositories;
+
+import com.industriaspleneo.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+}
