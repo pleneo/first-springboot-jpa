9a348a352afda9180cbdcf0747690bb8714a83f2
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 19:31:10 -0300

    created OrderItem JoinColumn to Many-to-Many association

--- src/main/java/com/industriaspleneo/course/entities/Category.java
+++ src/main/java/com/industriaspleneo/course/entities/Category.java
@@ -12,7 +12,7 @@ import java.util.Set;
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/industriaspleneo/course/entities/Product.java
+++ src/main/java/com/industriaspleneo/course/entities/Product.java
@@ -10,7 +10,7 @@ import java.util.Set;
 @Entity
 @Table(name="tb_product")
 public class Product implements Serializable {
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
