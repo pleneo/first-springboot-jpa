c858beb92046f1e70fb464b73c6d21e32ea484c0
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 13:27:57 -0300

    created order table using iso 8601

--- src/main/java/com/industriaspleneo/course/entities/User.java
+++ src/main/java/com/industriaspleneo/course/entities/User.java
@@ -3,6 +3,8 @@ package com.industriaspleneo.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    //association, if is a collection, you should instatiate it.
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +74,10 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
