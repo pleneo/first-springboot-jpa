c258abd0cc8796d28c9d43e5a36486f41ee54569
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 11:46:29 -0300

    added User and UserResource

---
+++ src/main/java/com/industriaspleneo/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.industriaspleneo.course.resources;
+
+import com.industriaspleneo.course.entities.User;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Ney", "ney@ney.com", "99999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
