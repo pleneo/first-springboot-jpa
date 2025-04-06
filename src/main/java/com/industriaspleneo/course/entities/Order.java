e10b685bf831d31768ba0f3676ac4b3f5c8d0afa
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 14:15:41 -0300

    created OrderStatus enum

--- src/main/java/com/industriaspleneo/course/entities/Order.java
+++ src/main/java/com/industriaspleneo/course/entities/Order.java
@@ -2,6 +2,7 @@ package com.industriaspleneo.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.industriaspleneo.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     //associations
     @ManyToOne
     @JoinColumn(name= "client_id")
@@ -28,9 +31,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +61,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus!=null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
