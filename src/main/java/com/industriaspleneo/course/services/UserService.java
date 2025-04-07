7bb6d023c4eedde8e9a98a12a545bf9bc7d33d17
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 21:49:03 -0300

    created exception handlers for findById method.

--- src/main/java/com/industriaspleneo/course/services/UserService.java
+++ src/main/java/com/industriaspleneo/course/services/UserService.java
@@ -2,10 +2,12 @@ package com.industriaspleneo.course.services;
 
 import com.industriaspleneo.course.entities.User;
 import com.industriaspleneo.course.repositories.UserRepository;
+import com.industriaspleneo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +24,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
