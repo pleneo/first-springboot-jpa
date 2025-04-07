29a202effc336fa5764fdba7dc925af94c8f2a6e
Plinio Gomes <plinioigt@gmail.com>
2025-04-06 21:24:01 -0300

    made user update method

--- src/main/java/com/industriaspleneo/course/services/UserService.java
+++ src/main/java/com/industriaspleneo/course/services/UserService.java
@@ -32,5 +32,16 @@ public class UserService {
     public void delete(Long id){
         repository.deleteById(id);
     }
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id); //mais eficiente, só prepara o obj, nao faz consulta ao banco. bom pra update
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 
 }
